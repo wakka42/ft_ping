ebc6be118d141133d84093f367ce129970d73a24
Wakka <david.asli.perso@gmail.com>
2026-08-01 20:28:39 +0200

    Creating a socket that will use the ICMP protocol for the ping function

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/ip_icmp.h>
+
+int main(int ac, char **av)
+{
+    (void)ac;
+    (void)av;
+    int psocket = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+    if (psocket == -1){
+        printf("An error occured when initializing a socket\n");
+        return 1;
+    }
+    printf("Socket ready\n");
+    return (0);
+}
